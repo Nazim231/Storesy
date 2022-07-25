60f66238daae59ef41c55af8620e9dcbc1ef0491
Nazim <nsn172002.nazim@gmail.com>
2022-07-25 12:14:27 +0530

    Navigation Drawer Added with Fragments

--- app/src/main/java/com/naztec/storesy/MainActivity.java
+++ app/src/main/java/com/naztec/storesy/MainActivity.java
@@ -1,14 +1,85 @@
 package com.naztec.storesy;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.naztec.storesy.Fragments.*;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navView;
+    FrameLayout frameLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        frameLayout = findViewById(R.id.main_frame_layout);
+        setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navView.setNavigationItemSelectedListener(this);
+
     }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case R.id.nav_cart:
+                loadFragment(new CartFragment());
+                break;
+            case R.id.nav_wishlist:
+                loadFragment(new WishlistFragment());
+                break;
+            case R.id.nav_orders:
+                loadFragment(new OrdersFragment());
+                break;
+            case R.id.nav_account:
+                loadFragment(new AccountFragment());
+                break;
+            default:
+                loadFragment(new HomeFragment());
+                break;
+
+        }
+
+        return true;
+
+    }
+
+    /**
+     * Function to Set Fragment in the MainActivity FrameLayout
+     *
+     * @param fragment Fragment Object that needs to be shown to the user
+     */
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, fragment)
+                .commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
 }
